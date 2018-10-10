4e75f38d1e6514f8dfdba9264e035c0a47a1edef
DanBergelandPillar <dbergeland@pillartechnology.com>
2018-10-10 08:31:16 -0500

    adds Rates

--- src/family.py
+++ src/family.py
@@ -1,3 +1,9 @@
 class Family():
     def __init__(self):
         self.rates = []
+
+class Rate():
+    def __init__(self, start_int, stop_int, pay_rate):
+        self.start_int = start_int
+        self.stop_int = stop_int
+        self.pay_rate = pay_rate
--- src/family_test.py
+++ src/family_test.py
@@ -4,9 +4,11 @@ import family
 class FamilyTest(unittest.TestCase):
     def setUp(self):
         self.fam = family.Family()
+        self.rate = family.Rate(0,1,15.)
 
-    def test(self):
-        self.assertIsNotNone(self.fam)
+    def test_constructors(self):
+        self.assertEqual(self.fam.rates, [])
+        self.assertEqual(self.rate.pay_rate, 15.)
 
 if __name__ == '__main__':
     unittest.main()
