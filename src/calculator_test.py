96b4a36ce64e5affdaaf028dec9f1ac334dadb9c
DanBergelandPillar <dbergeland@pillartechnology.com>
2018-10-10 09:17:15 -0500

    adds rate calculation for Family A

--- src/calculator_test.py
+++ src/calculator_test.py
@@ -8,11 +8,15 @@ class CalculatorTest(unittest.TestCase):
         rate = calculator.Rate(0,1,15.)
         self.assertEqual(rate.pay_rate, 15.)
         self.assertEqual(rate.start_int, 0)
-        self.assertEqual(rate.stop_int, 1)
+        self.assertEqual(rate.end_int, 1)
 
     def test_FamilyAPays15perHourFrom5pmTo11pm(self):
         wage = calculator.calculate('5pm','11pm',constants.FAMILY_A)
         self.assertEqual(wage, 6*15.)
+
+    def test_FamilyAPays20perHourFrom11pmTo4am(self):
+        wage = calculator.calculate('11pm','4am',constants.FAMILY_A)
+        self.assertEqual(wage, 5*20.)
 
 if __name__ == '__main__':
     unittest.main()
--- src/constants.py
+++ src/constants.py
@@ -4,6 +4,6 @@ HOURS = ['5pm', '6pm', '7pm', '8pm', '9pm', '10pm', '11pm', '12pm', '1am', '2am'
 
 FAMILY_A = [
     Rate(0,HOURS.index('11pm'),15.), 
-    Rate(HOURS.index('11pm'), len(HOURS), 20.)
+    Rate(HOURS.index('11pm'), len(HOURS) - 1, 20.)
     ]
 
